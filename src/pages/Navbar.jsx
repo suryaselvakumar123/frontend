8e23410fd1141332d13cd633bcdbddf08454b002
suryaselvakumar123 <suryaselvakumar1007@gmail.com>
2023-11-19 13:14:18 +0530

    update

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -133,7 +133,7 @@ const Navbar = () => {
                                 duration={500}
                                 onClick={homeHandler}
                             >
-                                SHOPCART
+                                SURYA-CART
                             </NavLogo>
                         </Typography>
                     </HomeContainer>
@@ -214,7 +214,7 @@ const Navbar = () => {
                             >
                                 <LocalMallIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
 
-                                SHOPCART
+                                SURYA-CART
                             </NavLogo>
                         </Typography>
                     </HomeContainer>
